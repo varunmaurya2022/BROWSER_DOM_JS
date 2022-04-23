9b83301a15c1b68284c232e6633f19abf169b9e4
varun maurya <varunmaurya2018@gmail.com>
2022-04-23 17:29:43 +0530

    Add index.js

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🥖Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// console.log(document.querySelector('.guess').value);[
+
+// document.querySelector('.guess').value = 23;
+
+
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = '😒No Number'
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '😉 Correct Number!'
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '.🎈To High!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '🧨You Lost The Game';
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = '.🎈To Low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '🧨You Lost The Game';
+            document.querySelector('.score').textContent = 0;
+        }
+
+    }
+
+    // document.querySelector('.message').textContent = '😀Correct Number';
+});
